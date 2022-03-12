1abb3f6d4237ddeeef3ba8c75d7d70ef5af9b262
tomershafir <tomer.shafir8@gmail.com>
2022-03-12 10:55:34 +0200

    Made stdio functions more C-like

--- lib/io/stdio.sh
+++ lib/io/stdio.sh
@@ -7,46 +7,6 @@ export readonly BASH_STOUT_FILE_DESCRIPTOR=1
 export readonly BASH_STDERR_FILE_DESCRIPTOR=2
 
 #######################################
-# The function outputs the accepted arguments to stdout while overriding existing stdout data.
-#
-# Globals:
-#   BASH_STOUT_FILE_DESCRIPTOR
-# Arguments:
-#   A list of data elements to write to stdout
-# Outputs:
-#   Writes the accepted arguments to stdout
-# Returns:
-#   0 if operation succeeded, non-zero otherwise
-#######################################
-stdio::stdout() {
-    if ! echo "$@" > "${BASH_STOUT_FILE_DESCRIPTOR}"; then 
-        append_to_stderr "(error): Failed to write to stdout."
-        return 1
-    fi
-    return 0
-}
-
-#######################################
-# The function outputs the accepted arguments to stderr while overriding existing stderr data.
-#
-# Globals:
-#   BASH_STERR_FILE_DESCRIPTOR
-# Arguments:
-#   A list of data elements to write to stderr
-# Outputs:
-#   Writes the accepted arguments to stderr
-# Returns:
-#   0 if operation succeeded, non-zero otherwise
-#######################################
-stdio::stderr() {
-    if ! echo "$@" > "${BASH_STDERR_FILE_DESCRIPTOR}"; then 
-        append_to_stderr "(error): Failed to write to stderr."
-        return 1
-    fi
-    return 0
-}
-
-#######################################
 # The function appends the accepted arguments to stdout.
 #
 # Globals:
@@ -58,9 +18,8 @@ stdio::stderr() {
 # Returns:
 #   0 if operation succeeded, non-zero otherwise
 #######################################
-stdio::append_to_stdout() {
+stdio::print() {
     if ! echo "$@" &> "${BASH_STOUT_FILE_DESCRIPTOR}"; then 
-        append_to_stderr "(error): Failed to append to stdout."
         return 1
     fi
     return 0
@@ -78,9 +37,8 @@ stdio::append_to_stdout() {
 # Returns:
 #   0 if operation succeeded, non-zero otherwise
 #######################################
-stdio::append_to_stderr() {
+stdio::printerr() {
     if ! echo "$@" &> "${BASH_STDERR_FILE_DESCRIPTOR}"; then 
-        append_to_stderr "(error): Failed to write to stderr."
         return 1
     fi
     return 0
