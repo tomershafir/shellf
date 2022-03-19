4dc9e9acc1045e5709435789bdb567d4a8d94b01
tomershafir <tomer.shafir8@gmail.com>
2022-03-19 17:42:44 +0200

    Removed redirections

--- lib/io/stdio.sh
+++ lib/io/stdio.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 #
 # Bash library that supplies standard I/O utilities.
+#######################################
 
 export readonly BASH_STDIN_FILE_DESCRIPTOR=0
-export readonly BASH_STOUT_FILE_DESCRIPTOR=1
+export readonly BASH_STDOUT_FILE_DESCRIPTOR=1
 export readonly BASH_STDERR_FILE_DESCRIPTOR=2
+
+readonly ERROR="ERROR:"
+readonly INFO="INFO:"
+readonly DEBUG="DEBUG::"
+readonly WARNING="WARNING:"
 
 #######################################
 # The function appends the accepted arguments to stdout.
@@ -19,21 +25,83 @@ export readonly BASH_STDERR_FILE_DESCRIPTOR=2
 #   0 if operation succeeded, non-zero otherwise
 #######################################
 stdio::print() {
-    echo "$@" &> "${BASH_STOUT_FILE_DESCRIPTOR}"
+    for val in "$@"; do
+        echo "${val}"
+    done
 }
 
 #######################################
-# The function appends the accepted arguments to stderr.
+# The function appends the accepted error input to stdout with an error prefix.
 #
 # Globals:
-#   BASH_STERR_FILE_DESCRIPTOR
+#   ERROR
+#   BASH_STDOUT_FILE_DESCRIPTOR
 # Arguments:
-#   A list of data elements to write to stderr
+#   A list of error data elements to write to stdout
 # Outputs:
-#   Appends the accepted arguments to stderr
+#   Appends the accepted arguments to stdout
 # Returns:
 #   0 if operation succeeded, non-zero otherwise
 #######################################
 stdio::printerr() {
-    echo "$@" &> "${BASH_STDERR_FILE_DESCRIPTOR}"
+    for val in "$@"; do
+        echo "${ERROR} ${val}"
+    done
+}
+
+#######################################
+# The function appends the accepted info input to stdout with an info prefix.
+#
+# Globals:
+#   INFO
+#   BASH_STDOUT_FILE_DESCRIPTOR
+# Arguments:
+#   A list of info data elements to write to stdout
+# Outputs:
+#   Appends the accepted arguments to stdout
+# Returns:
+#   0 if operation succeeded, non-zero otherwise
+#######################################
+stdio::info() {
+    for val in "$@"; do
+        echo "${INFO} ${val}"
+    done
+}
+
+#######################################
+# The function appends the accepted warning input to stdout with a warning prefix.
+#
+# Globals:
+#   WARNING
+#   BASH_STDOUT_FILE_DESCRIPTOR
+# Arguments:
+#   A list of warning data elements to write to stdout
+# Outputs:
+#   Appends the accepted arguments to stdout
+# Returns:
+#   0 if operation succeeded, non-zero otherwise
+#######################################
+stdio::warn() {
+    for val in "$@"; do
+        echo "${WARNING} ${val}"
+    done
+}
+
+#######################################
+# The function appends the accepted debug input to stdout with a debug prefix.
+#
+# Globals:
+#   DEBUG
+#   BASH_STDOUT_FILE_DESCRIPTOR
+# Arguments:
+#   A list of debug data elements to write to stdout
+# Outputs:
+#   Appends the accepted arguments to stdout
+# Returns:
+#   0 if operation succeeded, non-zero otherwise
+#######################################
+stdio::debug() {
+    for val in "$@"; do
+        echo "${DEBUG} ${val}"
+    done
 }
